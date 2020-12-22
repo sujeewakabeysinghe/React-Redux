aa7eb498be96ce8ca002405fe2eb2b65da24fbbb
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-12-23 00:16:34 +0530

    first component

---
+++ src/components/firtsComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'  //rce
+ 
+export class firtsComponent extends Component {
+    render() {
+        return (
+            <div>
+                <button>Click</button>
+            </div>
+        )
+    }
+}
+
+export default firtsComponent
